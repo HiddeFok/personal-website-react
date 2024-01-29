8e496523685c4f4814db7b6f53e0f60a501298c6
Hidde Fokkema (UvA) <h.j.fokkema@uva.nl>
2024-01-29 13:45:43 +0100

    Updated publications

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -12,16 +12,15 @@ export const listPublications: Publication[] = [
 {
     title: 'Risks of Recourse in Binary Classification', 
     authors: ['HF', 'Damien Garreau', 'Tim van Erven'],
-    journal: 'ArXiv', 
-    date: '2023', 
+    journal: 'Conference on Artificial Intelligence & Statistics (AISTATS)', 
+    date: '2024', 
     link: 'https://arxiv.org/abs/2306.00497', 
-    status: "preprint",
-    additional: 'Submitted'
+    status: "published",
   },
   {
     title: 'Attribution-based Explanations that Provide Recourse Cannot be Robust', 
     authors: ['HF', 'Rianne de Heide', 'Tim van Erven'],
-    journal: 'Journal of Machine Learning Research', 
+    journal: 'Journal of Machine Learning Research (JMLR)', 
     date: '2023', 
     link: 'https://jmlr.org/papers/v24/23-0042.html', 
     status: "published"
